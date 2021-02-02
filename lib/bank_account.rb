13661aeffd4449e3657f21c0c9cd923c27e72ca1
Carrie Jones <c.jessica.jones@gmail.com>
2021-02-02 15:32:30 +0000

    Automatically backed up by Learn

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,11 +1,12 @@
 class BankAccount
 
-  attr_accessor :status, :balance
+  attr_accessor :status, :balance, :previous_balance
   attr_reader :name
 
   def initialize(name)
     @name = name
     @balance = 1000
+    @previous_balance = 1000
     @status = "open"
   end
   
@@ -13,5 +14,23 @@ class BankAccount
     @name = name
   end
   
-
+  def deposit(amt)
+    balance = @balance + amt
+    @previous_balance = @balance
+    @balacnce = balance
+  end
+  
+  def display_balance
+    puts "$#{@balance}"
+  end
+  
+  def valid?
+    if @status = "open"
+      if @balance > 0
+        true
+      end
+    end
+    false
+  end
+  
 end
